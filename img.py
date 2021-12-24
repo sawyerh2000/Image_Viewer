568f741faa48ac6d4cb159750ac81dfd72b68cf1
Sawyer Hughes <sawyerh2000@gmail.com>
2021-12-23 21:01:01 -0500

    Runtime installation of PIL

--- img.py
+++ img.py
@@ -1,6 +1,15 @@
-import PIL.Image as Image
+#import PIL.Image as Image
 import io
 import sql
+import pip
+import importlib
+
+def runtime_installer(package):
+    try:
+        return importlib.import_module(package) 
+    except:
+        pip.main(["install", package])
+    return importlib.import_module(package)
 
 #method to convert image to bytes:
 def getBytes(path):
@@ -12,9 +21,11 @@ def getBytes(path):
 #method to display image (takes byte tuple from database as arg):
 def showImage(byte_tuple): #bytes argument should be sql.getInfo() call
     bytes=io.BytesIO(byte_tuple[0][1]) #readable bytemap for image from tupple in list
-    img=Image.open(bytes) #create Image object using bytes
+    img=PIL.Image.open(bytes) #create Image object using bytes
     img.show() #display image
 
+if __name__ == '__main__':
+    PIL = runtime_installer('PIL')
 #driver loop - gets user input for choice 
 while True:
     choice = input("\n1) Get image\n2) Add image\n3) Get File names\n4) Quit\n")
